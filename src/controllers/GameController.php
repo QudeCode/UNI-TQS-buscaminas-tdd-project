39434e0376bc66a5030d2375c118112a3e87b464
Eric <eric.cruz.tomass22@gmail.com>
2023-11-25 19:33:16 +0100

    Primeros tests tdd y empezado desarrollo de gamecontroller. Readme ampliado. Ejecución automática de tests imolementada

--- src/controllers/GameController.php
+++ src/controllers/GameController.php
@@ -1,10 +1,23 @@
 <?php
+
+namespace Controllers;
+
+use Models\Grid;
+
 require_once __DIR__ . '/../models/Cell.php';
 require_once __DIR__ . '/../models/Grid.php';
 
-require __DIR__ . '/../views/game.php';
-
 class GameController
 {
+    public function __construct()
+    {
+    }
 
+    public function createGrid()
+    {
+        return new Grid;
+    }
 }
+
+
+require __DIR__ . '/../views/game.php';
